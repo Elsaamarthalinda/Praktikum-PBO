d09d6ff424ff0487e8285b5a7c0e4e668feb01b0
Elsa Marthalinda <elsa29389@gmail.com>
2026-09-11 20:45:18 +0700

    Tugas mandiri jobsheet 3

--- id/ac/polinema/Account.java
+++ id/ac/polinema/Account.java
@@ -1,13 +1,18 @@
 package id.ac.polinema;
 public class Account {
+
+    //private membuat atribut hanya dapat diakses  didalam class account
     private String accountNumber;
     private String ownerName;
     private double balance;
+    private double dailyWithdrawalLimit; //modif tugas mandiri
 
-    public Account(String accountNumber, String ownerName, double balance) {
+    //modif tugas mandiri
+    public Account(String accountNumber, String ownerName, double balance, double dailyWithdrawalLimit) {
         this.accountNumber = accountNumber;
         this.ownerName = ownerName;
         this.balance = balance;
+        this.dailyWithdrawalLimit = dailyWithdrawalLimit;
     }
 
     public String getAccountNumber() {
@@ -22,7 +27,7 @@ public class Account {
         return balance;
     }
 
-    public boolean deposit(double amount) {
+    public boolean deposit(double amount) { // jika uang jika kurang dari sama dengan 0 maka return false
         if (amount <= 0) {
             return false;
         }
@@ -31,7 +36,7 @@ public class Account {
     }
 
    public boolean withdraw(double amount) {
-        if (amount <= 0 || amount > balance) {
+        if (amount <= 0 || amount > balance || amount > dailyWithdrawalLimit) { //modif tugas mandiri
             return false;
         }
         balance -= amount;
--- id/ac/polinema/Main.java
+++ id/ac/polinema/Main.java
@@ -1,8 +1,14 @@
 package id.ac.polinema;
 public class Main {
     public static void main(String[] args) {
-        Account acc = new Account("A001", "Nadia", 500000);
-        acc.withdraw(150000);
-        acc.printInfo();
+
+        //membuat objek account dengan no rekening, nama pemilik, saldo awal, dan batas penarikan harian
+        Account limited = new Account("A005", "Fajar", 1000000, 200000);
+
+        // penarikan 300000 melebihi batas penarikan harian, sehingga gagal
+        System.out.println("Withdraw 300000 allowed? " + limited.withdraw(300000));
+        // penarikan 150000 masih dalam batas penarikan harian, sehingga berhasil
+        System.out.println("Withdraw 150000 allowed? " + limited.withdraw(150000) );
+        limited.printInfo();
     }
 }
